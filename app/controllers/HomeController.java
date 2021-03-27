53ea52fdd5f141f20fafa40e1405210d1be657dc
Railson Santos <nosliarpb@hotmail.com>
2021-03-27 08:23:06 -0300

    Inserindo Autorização e Chamando os dados de contributors da organização

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -14,6 +14,8 @@ import java.util.*;
 import java.util.Scanner;
 import play.libs.Json;
 import org.jsoup.Jsoup;
+import com.fasterxml.jackson.databind.*;
+import java.io.*;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -22,14 +24,27 @@ import org.jsoup.Jsoup;
 public class HomeController extends Controller {
 		private String error;
 		private String variavelDeAmbiente = System.getenv("GH_TOKEN");
+		private String contributors;
 
 
 	    public Result index(String orgName) {
+	    	ObjectMapper objectMapper = new ObjectMapper();
 	    	try{
 	    		
-                String json = Jsoup.connect("https://api.github.com/users/"+orgName+"/repos").ignoreContentType(true).execute().body();
+                String json = Jsoup.connect("https://api.github.com/users/"+orgName+"/repos")
+                .header("Authorization", variavelDeAmbiente)
+                .ignoreContentType(true).get().body().text();
+
+                JsonNode nodeJson = objectMapper.readValue(json, JsonNode.class);
+                System.out.println(nodeJson.get(0).get("contributors_url"));
+
+                String js = Jsoup.connect((nodeJson.get(0).get("contributors_url")+"").replaceAll("^\"|\"$", ""))
+                .header("Authorization", variavelDeAmbiente)
+                .ignoreContentType(true).get().body().text();
+
+
 	    	  
-	    	 return ok(json);
+	    	 return ok(js);
 	    	} catch(Exception e){
 	    		error = e+"";
 	    	}
